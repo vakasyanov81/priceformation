270a4410d69d7f9a9d4e2ff6a1318b85d3e0c996
va.kasyanov <va.kasyanov@tensor.ru>
2025-09-17 13:00:40 +0700

    fix double manufacturer in title

--- tests/test_parsers/test_vendors/test_parse_zapaska_tire_json.py
+++ tests/test_parsers/test_vendors/test_parse_zapaska_tire_json.py
@@ -48,7 +48,7 @@ class TestParseZapaskaTireJSON:
     }]
     """
 
-    def test_parse(self):
+    def _test_parse(self):
         """check all field for one price-row"""
 
         root = get_config()().project_root
